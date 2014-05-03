a26d93a19a277c9400bb599e496df23edbcbb3a7
Andrew Jansen <backup@TheCowMan.local>
2014-05-03 13:02:10 -0700

    m

--- GatsbiServer/src/gatsbiserver/GatsbiServer.java
+++ GatsbiServer/src/gatsbiserver/GatsbiServer.java
@@ -6,6 +6,8 @@ package gatsbiserver;
 
 import java.io.*;
 import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class GatsbiServer {
 
@@ -15,6 +17,9 @@ public class GatsbiServer {
     static int portNumber = 9090;
 
     public static void main(String[] args) {
+        try {
+            System.out.println(InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException ex) {}
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             while (true) {
                 new SocketThread(serverSocket.accept()).start();
